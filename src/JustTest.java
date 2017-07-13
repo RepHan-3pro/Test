b17645898e897f7a12bee57b5dea87b6c74357d5
rephan <reph@ndscd.com>
2017-07-13 17:14:00 +0800

    test

--- src/JustTest.java
+++ src/JustTest.java
@@ -5,36 +5,57 @@ import java.io.IOException;
  */
 public class JustTest {
     public void test() throws IOException {
-        String test=new String("hello world");
+        String test = new String("hello world");
         System.out.println(test);
-         System.out.println();
-   System.out.println();
-  System.out.println();
-  System.out.println();
-  System.out.println();
-  System.out.println();
-  System.out.println();
-  System.out.println();
-  System.out.println();
-  System.out.println();
-  StringBuffer testt=new StringBuffer();
-  System.out.println();
-  System.out.println(); System.out.println(); System.out.println();
- System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        StringBuffer testt = new StringBuffer();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
         System.out.println();
 
     }
+
     public void Test() throws IOException {
         test();
     }
+
     public void Test1() throws IOException {
         test();
     }
+
     public void Test2() throws IOException {
         test();
     }
+
     public void Test3() throws IOException {
         test();
     }
 
+    public void Test4() throws IOException {
+        test();
+    }
+
+    public void Test5() throws IOException {
+        test();
+    }
+
+    public void Test6() throws IOException {
+        test();
+    }
+
+    public void Test7() throws IOException {
+        test();
+    }
 }
