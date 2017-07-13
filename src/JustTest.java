10c25c194fc6e72a4fefd03dd11983ea14e80cf5
rephan <reph@ndscd.com>
2017-07-13 16:59:02 +0800

    test

--- src/JustTest.java
+++ src/JustTest.java
@@ -1,17 +1,9 @@
-import org.junit.Test;
-
-import java.io.File;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.nio.channels.Channel;
-import java.util.Arrays;
 
 /**
  * Created by reph on 2017/6/29.
  */
 public class JustTest {
-    @Test
     public void test() throws IOException {
         String test=new String("hello world");
         System.out.println(test);
